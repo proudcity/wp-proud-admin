13d7dff5b1882c071a6fe42ec51ae6dfd5a0a118
Alex Schmoe <aschmoe@gmail.com>
2015-12-17 15:54:23 -0800

    updating to use proudcore

--- wp-proud-admin.php
+++ wp-proud-admin.php
@@ -11,24 +11,49 @@ License:            MIT License
 License URI:        http://opensource.org/licenses/MIT
 */
 
+namespace Proud\Admin;
+
 require_once get_template_directory() . '/lib/assets.php';
 
 use Proud\Theme\Assets;
 
-//add css
-function proud_admin_theme_style() {
-  // Bootstrap + proud-library styles from theme
-  wp_enqueue_style('proud-vendor/css', Assets\asset_path('styles/proud-vendor.css'), false, null);
-
-  // Local
-  $path = plugins_url('dist/',__FILE__);
-  wp_enqueue_style('proud-admin/css', $path . 'styles/proud-admin.css', false, null);
-  wp_enqueue_script('proud-admin/js', $path . 'scripts/proud-admin.js', ['proud','jquery'], null, true);
-  // // Bootstrap
-  // wp_enqueue_script('proud/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+// Load Extendible
+// -----------------------
+if ( ! class_exists( 'ProudPlugin' ) ) {
+  require_once( plugin_dir_path(__FILE__) . '../wp-proud-core/proud-plugin.class.php' );
 }
-add_action('admin_enqueue_scripts', 'proud_admin_theme_style');
-add_action('login_enqueue_scripts', 'proud_admin_theme_style');
+
+class ProudAdmin extends \ProudPlugin {
+
+  /**
+   * Constructor
+   */
+  public function __construct() {
+
+    parent::__construct( array(
+      'textdomain'     => 'wp-proud-admin',
+      'plugin_path'    => __FILE__,
+    ) );
+
+    $this->hook( 'admin_enqueue_scripts', 'proud_admin_theme_style' );
+    $this->hook( 'login_enqueue_scripts', 'proud_admin_theme_style' );
+  }
+
+  //add css
+  function proud_admin_theme_style() {
+    // Bootstrap + proud-library styles from theme
+    wp_enqueue_style('proud-vendor/css', Assets\asset_path('styles/proud-vendor.css'), false, null);
+
+    // Local
+    $path = plugins_url('dist/',__FILE__);
+    wp_enqueue_style('proud-admin/css', $path . 'styles/proud-admin.css', false, null);
+    wp_enqueue_script('proud-admin/js', $path . 'scripts/proud-admin.js', ['proud','jquery'], null, true);
+    // // Bootstrap
+    // wp_enqueue_script('proud/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  }
+}
+
+new ProudAdmin;
 
 //login screen
 include_once('login/login.php');
