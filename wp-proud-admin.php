d1986199448a929d6a9881f7bb376cddbbce3a5d
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-07 00:35:10 -0800

    Tweaking admin bars, dashboard content

--- wp-proud-admin.php
+++ wp-proud-admin.php
@@ -37,6 +37,11 @@ class ProudAdmin extends \ProudPlugin {
 
     $this->hook( 'admin_enqueue_scripts', 'proud_admin_theme_style' );
     $this->hook( 'login_enqueue_scripts', 'proud_admin_theme_style' );
+    $this->hook( 'admin_bar_menu', 'wp_admin_bar_dashboard', 20 );
+    $this->hook( 'admin_bar_menu', 'wp_admin_bar_account', 11 );
+    $this->hook( 'wp_dashboard_setup', 'remove_dashboard_meta' );
+    $this->hook( 'admin_footer_text', 'dashboard_footer' );
+
   }
 
   //add css
@@ -51,6 +56,69 @@ class ProudAdmin extends \ProudPlugin {
     // // Bootstrap
     // wp_enqueue_script('proud/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
   }
+
+  // Add Dashboard link
+  function wp_admin_bar_dashboard($admin_bar) {
+  //echo "<pre>";
+  //print_r($admin_bar);
+  //echo "<pre>";
+   $admin_bar->add_menu( array(
+        'id'    => 'my-item',
+        'parent' => 'root-default',
+        'title' => 'Dashboard',
+        'href'  => get_site_url() . '/wp-admin',
+        'weight' => 10,
+        'meta'  => array(
+            'title' => __('My Item'),
+        ),
+    ) );
+  }
+
+  // Change the "Howdy" text
+  function wp_admin_bar_account( $wp_admin_bar ) {
+    $user_id = get_current_user_id();
+    $current_user = wp_get_current_user();
+    $profile_url = get_edit_profile_url( $user_id );
+
+    if ( 0 != $user_id ) {
+      /* Add the "My Account" menu */
+      $avatar = get_avatar( $user_id, 28 );
+      $howdy = $current_user->user_email;//sprintf( __('Welcome, %1$s'), $current_user->display_name );
+      $class = empty( $avatar ) ? '' : 'with-avatar';
+
+      $wp_admin_bar->add_menu( array(
+        'id' => 'my-account',
+        'parent' => 'top-secondary',
+        'title' => $howdy . $avatar,
+        'href' => $profile_url,
+        'meta' => array(
+        'class' => $class,
+        ),
+      ) );
+
+    }
+  }
+
+  function remove_dashboard_meta() {
+    //$user = wp_get_current_user();
+    //if ( ! $user->has_cap( 'manage_options' ) ) {
+        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+    //}
+  }
+
+  function dashboard_footer () {
+    $url = get_site_url();
+    echo "<a href='http://proudcity.com' target='_blank'>ProudCity</a> is proudly powered by <a href='http://wordpress.com' target='_blank'>WordPress</a> and Open Source software. <a href='$url/wp-admin/credits.php'>Credits</a> &middot; <a href='$url/wp-admin/freedoms.php'>Freedoms</a>.";
+  }
+
 }
 
 new ProudAdmin;
@@ -58,3 +126,16 @@ new ProudAdmin;
 //login screen
 include_once('login/login.php');
 include_once('login/add_menu.php');
+
+
+
+// @todo: move
+function add_theme_caps() {
+    // gets the author role
+    $role = get_role( 'author' );
+
+    // This only works, because it accesses the class instance.
+    // would allow the author to edit others' posts for current theme only
+    $role->add_cap( 'edit_others_posts' ); 
+}
+add_action( 'admin_init', 'add_theme_caps');
