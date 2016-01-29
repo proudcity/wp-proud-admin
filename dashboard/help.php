21555395f88ee7fafe70bc2696e5d8bf2b74f28a
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-29 01:19:03 -0800

    pagebuilder popup, customizer style changes

--- dashboard/help.php
+++ dashboard/help.php
@@ -3,6 +3,8 @@
 
     <a href="https://proudcity.com/support/create"><i class="fa fa-2x fa-sticky-note-o"></i> Create a ticket</a>
 
+    <a href="https://proudcity.com/request-feature"><i class="fa fa-2x fa-plus-circle"></i> Feature request</a>
+
     <a href="https://proudcity.com/guides"><i class="fa fa-2x fa-binoculars"></i> Guides</a>
   <div class="clearfix"></div>
 </div>
