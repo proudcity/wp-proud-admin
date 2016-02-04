22d2d784b6c7b370d77535ddaab2eb6e0bada559
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-03 20:53:04 -0800

    Fixing city google places api code

--- settings/settings.php
+++ settings/settings.php
@@ -66,13 +66,15 @@ class ProudGeneralSettingsPage
         'search_title' => [
           '#type' => 'html',
           '#html' => 
-            "<script type='text/javascript'>google.maps.event.addDomListener(window, 'load', function () {
-              var places = new google.maps.places.Autocomplete(document.getElementById('city_input'), options);
-              google.maps.event.addListener(places, 'place_changed', function () {
-                $('#city-input-wrapper-header').addClass('active');
-                var place = places.getPlace();
-                console.log(place);
-                //window.location = 'https://demo.proudcity.com/get/' + place.address_components[2].long_name + '/' + place.address_components[0].long_name;
+            "<script type='text/javascript'>(function($) {
+              google.maps.event.addDomListener(window, 'load', function () {
+                var places = new google.maps.places.Autocomplete(document.getElementById('city_input'), options);
+                google.maps.event.addListener(places, 'place_changed', function () {
+                  $('#city-input-wrapper-header').addClass('active');
+                  var place = places.getPlace();
+                  console.log(place);
+                  //window.location = 'https://demo.proudcity.com/get/' + place.address_components[2].long_name + '/' + place.address_components[0].long_name;
+                });
               });
             });</script>",
         ],
