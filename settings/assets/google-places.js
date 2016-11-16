631996d7a3c1a2a2e616665cc1349187423732d9
Jeff Lyon <jeff@albatrossdigital.com>
2016-11-16 15:11:32 -0800

    Adding test for google places locations

--- settings/assets/google-places.js
+++ settings/assets/google-places.js
@@ -8,6 +8,7 @@
     google.maps.event.addListener(places, 'place_changed', function () {
       $('#city-input-wrapper-header').addClass('active');
       var place = places.getPlace();
+      //console.log(place.photos[0].getUrl({'maxWidth': 2000, 'maxHeight': 1200}));
       $('#city').val(place.address_components[0].long_name);
       $('#state').val(place.address_components[2].types[0] == 'administrative_area_level_1' ? place.address_components[2].long_name : place.address_components[3].long_name);
       $('#lat').val(place.geometry.location.lat());
