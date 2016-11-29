d689ee49c3b5e91829c3aeb59564f1b11927c962
Alex Schmoe <aschmoe@gmail.com>
2016-11-29 11:26:16 -0800

    adding extendable helper classes for page settings and meta boxes

---
+++ lib/meta-box.class.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * @author ProudCity
+ */
+
+abstract class ProudMetaBox {
+
+    public $options = []; // Holds the values to be used in the fields callbacks, MUST be overridden
+    private $key; // for forms, nonce, ect
+    public $post; // Content for meta fields
+    public $fields; // Form fields
+    public $form; // FormHelper
+    public $id; // metabox id 
+    public $title; // metabox title
+    public $screen; // metabox screen
+    public $position; // metabox position
+    public $priority; // metabox priority
+
+    /**
+     * Start up
+     * @param string $key
+     * @param string $title // metabox title
+     * @param string $screen // metabox screen
+     * @param string  $position // metabox position
+     * @param string $priority // metabox priority
+     */
+    public function __construct( $key, $title, $screen = null, $position = 'advanced', $priority = 'default' ) {
+      $this->key = $key;
+      $this->title = $title;
+      $this->screen = $screen;
+      $this->position = $position;
+      $this->priority = $priority;
+
+      // Add save option
+      add_action( 'save_post', array( $this, 'save_meta' ), 10, 3 );
+      add_action( 'admin_init', array( $this, 'register_box' ) );
+    }
+
+    /**
+     * Register box hook
+     */
+    public function register_box() {
+      add_meta_box( 
+        $this->key . '_meta_box',
+        $this->title,
+        array($this, 'settings_content'),
+        $this->screen, 
+        $this->position,
+        $this->priority
+      );
+      // Set fields, no display
+      $this->set_fields( false );
+      $this->form = new \Proud\Core\FormHelper( $this->key, $this->fields, 1, 'form' );
+    }
+
+    /**
+     * Called on form creation
+     * @param $displaying : false if just building form, true if about to display
+     * Use displaying:true to do any difficult loading that should only occur when
+     * the form actually will display
+     */
+    abstract protected function set_fields( $displaying );
+
+    /** 
+     * Rebuilds form values from options
+     */
+    public function build_options( $id = null ) {
+      if( !$id ) {
+        $id = $this->post->ID;
+      }
+      $meta = get_post_meta( $id );
+      foreach ( $this->options as $option => $default ) {
+        if( isset( $meta[$option][0] ) ) {
+          $this->options[$option] = $meta[$option][0];
+        }
+      }
+    }
+
+    /**
+     * Returns build options
+     */
+    public function get_options( $id = null ) {
+      $this->build_options( $id );
+      return $this->options;
+    }
+
+    /**
+     * Gets field ids for fields
+     * @param array $fields, subset of fields to fetch
+     * in form {fieldname}_id => 'theId'
+     */
+    public function get_field_ids( $fields = [] ) {
+      if( empty( $fields ) ) {
+        $fields = array_keys( $this->fields );
+      }
+      $ids = [];
+      foreach ( $fields as $fieldname ) {
+        $ids[ $fieldname . '_id' ] = $this->form->get_field_id( $fieldname );
+      }
+      return $ids;
+    }
+
+    /**
+     * Gets field names for fields
+     * @param array $fields, subset of fields to fetch
+     * in form {fieldname}_name => 'theName'
+     */
+    public function get_field_names( $fields = [] ) {
+      if( empty( $fields ) ) {
+        $fields = array_keys( $this->fields );
+      }
+      $names = [];
+      foreach ( $fields as $fieldname ) {
+        $names[ $fieldname . '_name' ] = $this->form->get_field_name( $fieldname );
+      }
+      return $names;
+    }
+
+    /**
+     * Prints form fields
+     * @param bool $print_form: should the function print form immediately or return content?
+     */
+    public function print_form( $post ) {
+      // Set post
+      $this->post = $post;
+      // Set fields, displaying
+      $this->set_fields( true );
+      $this->build_options( );
+      // Print fields
+      $this->form->printFields( $this->options, $this->fields, 1, 'form' );
+    } 
+
+    /**
+     * Prints form
+     */
+    public function settings_content( $post ) {
+      $this->print_form( $post );
+    }
+
+    /** 
+     * Validate and return values
+     */
+    public function validate_values( $post ) {
+      // We have screen requirements not met 
+      if( !empty( $screen ) && $post->post_type !== $screen ) {
+        return false;
+      }
+      // Return values
+      return $this->form->getFormValues( $_POST );
+    }
+
+    /** 
+     * Runs through options, saves
+     */
+    public function save_all( $values, $post_id ) {
+      foreach ( array_keys( $this->options ) as $key ) {
+        if ( isset( $values[$key] ) ) {
+          update_post_meta( $post_id, $key, $values[$key] );
+        }
+      }
+    }
+
+    /** 
+     * Saves form values
+     */
+    public function save_meta( $post_id, $post, $update ) {
+      // Grab form values from Request
+      $values = $this->validate_values( $post );
+      if( !empty( $values ) ) {
+        $this->save_all( $values, $post_id );
+      }
+    }
+}
---
+++ lib/settings-page.class.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * @author ProudCity
+ */
+
+abstract class ProudSettingsPage {
+
+    public $options; // Holds the values to be used in the fields callbacks
+    public $option; // Optional option string to use array as basis
+    private $key; // for forms, nonce, ect
+    public $hook; // The menu callback
+    public $submenu; // Submenu options for admin menu
+    public $fields; // Form fields
+    public $form; // FormHelper 
+
+    /**
+     * Start up
+     * @param string $key
+     * @param array $submenu
+     *    with keys: parent_slug, page_title, menu_title, capability
+     * @param string $option (use single option array as basis)
+     * @param array $options (uses multiple options as basis for forms)
+     */
+    public function __construct( $key, $submenu, $option = '', $options = []  ) {
+      $this->key = $key;
+      $this->submenu = $submenu;
+      // Single mode or multiple?
+      if( $option ) {
+        $this->option = $option;
+        $this->options = [];
+      }
+      else {
+        $this->option = '';
+        $this->options = $options;
+      }
+      add_action( 'admin_menu', array( $this, 'create_menu' ) );
+    }
+
+    /**
+     * set_fields called on form creation
+     */
+    abstract protected function set_fields( );
+
+    /** 
+     * Rebuilds form values from options
+     */
+    public function build_options( ) {
+      // We're in single option mode
+      if( $this->option ) {
+        $this->options = get_option( $this->option );
+      }
+      // Array of options
+      else {
+        foreach ( $this->options as $option => $default ) {
+          $this->options[$option] = get_option( $option, $default ? $default : false );
+        }
+      }
+    }
+
+    /**
+     * Gets field ids for fields
+     * @param array $fields, subset of fields to fetch
+     * in form {fieldname}_id => 'theId'
+     */
+    public function get_field_ids( $fields = [] ) {
+      if( empty( $fields ) ) {
+        $fields = array_keys( $this->fields );
+      }
+      $ids = [];
+      foreach ( $fields as $fieldname ) {
+        $ids[ $fieldname . '_id' ] = $this->form->get_field_id( $fieldname );
+      }
+      return $ids;
+    }
+
+    /**
+     * Gets field names for fields
+     * @param array $fields, subset of fields to fetch
+     * in form {fieldname}_name => 'theName'
+     */
+    public function get_field_names( $fields = [] ) {
+      if( empty( $fields ) ) {
+        $fields = array_keys( $this->fields );
+      }
+      $names = [];
+      foreach ( $fields as $fieldname ) {
+        $names[ $fieldname . '_name' ] = $this->form->get_field_name( $fieldname );
+      }
+      return $names;
+    }
+
+    /** 
+     * Creates menu 
+     */
+    public function create_menu() {
+      // Extract submenu vars
+      extract($this->submenu);
+      // Menu page entry
+      if( !$parent_slug ) {
+        $hook = add_menu_page(
+          $page_title, 
+          $menu_title, 
+          $capability,
+          $this->key,
+          array( $this, 'settings_page' ),
+          $icon_url
+        );
+      }
+      // Submenu page entry
+      else {
+        $hook = add_submenu_page( 
+            $parent_slug, 
+            $page_title, 
+            $menu_title, 
+            $capability,
+            $this->key,
+            array( $this, 'settings_page' )
+        );
+      }
+      $this->hook = $hook;
+      // Add action to load our form
+      add_action( 'load-' . $hook, array( $this, 'load_form' ) );
+    }
+
+    /**
+     * Calls form actions and builds fields
+     */
+    public function load_form() {
+      $this->set_fields( );
+      $this->build_options( );
+      $this->form = new \Proud\Core\FormHelper( $this->key, $this->fields, 1, 'form' );
+    }
+
+    /**
+     * Deal with nonce, print page content
+     */
+    public function settings_page() {
+      // Do we have post?
+      if(isset($_POST['_wpnonce'])) {
+        if( wp_verify_nonce( $_POST['_wpnonce'], $this->key ) ) {
+          $this->save($_POST);
+        }
+      }
+      // Print content
+      $this->settings_content();
+    }
+
+    /**
+     * Prints form
+     * @param array $args: override default settins
+     */
+    public function print_form( $args = [] ) {
+      $this->build_options( );
+      $args = array_merge( array(
+        'button_text' => __pcHelp('Save'),
+        'method' => 'post',
+        'action' => '',
+        'instance' => $this->options,
+        'fields' => $this->fields
+      ), $args );
+      // Print form content
+      $this->form->printForm( $args );
+    }
+
+    /**
+     * build_options called on form creation
+     */
+    abstract protected function settings_content( );
+
+    /**
+     * Deals with submit values
+     */
+    public function escape_post_value( &$value, $field ) {
+      // If checkbox, and no value, set to 0
+      if( 'checkbox' === $field['#type'] && !isset( $value ) ) {
+        $value = 0;
+      }
+      // WYSIWYG
+      else if( 'editor' === $field['#type'] ) {
+        $value = wp_kses_post( $value );
+      }
+      // Array values (checkboxes)
+      else if( 'checkboxes' === $field['#type'] && is_array( $value ) ) {
+        foreach ( $value as $key => &$v ) {
+          $v = esc_attr($v);
+        }
+      }
+      // Groups of fields
+      else if( 'group' === $field['#type'] && is_array( $value ) ) {
+        foreach ( $value as $key => &$value_group ) {
+          foreach ($value_group as $field_key => &$field_value) {
+            if( isset( $field['#sub_items_template'][$field_key] ) ) {
+              // Recurse with field content
+              $this->escape_post_value( $field_value, $field['#sub_items_template'][$field_key] );
+            }
+            else {
+              unset( $value_group[$field_key] );
+            }
+          }
+        }
+      }
+      else {
+        $value = esc_attr( $value );
+      }
+    }
+
+    /** 
+     * Saves form values
+     */
+    public function save( &$raw_values ) {
+      $values = $this->form->getFormValues( $raw_values );
+      foreach ( array_keys( $this->options ) as $key ) {
+        // Escape
+        $this->escape_post_value( $values[$key], $this->fields[$key] );
+        // Multiple option mode ?
+        if ( !$this->option && isset( $values[$key] ) ) {
+          update_option( $key, $values[$key] );
+        }
+      }
+      // Single option mode
+      if( $this->option ) {
+        update_option( $this->option, $values );
+      }
+      $raw_values = $values;
+    }
+}
