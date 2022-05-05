57c9fdfefb82aab8a7f69940938f36f80c0e542a
Curtis McHale <curtis@curtismchale.ca>
2022-05-05 07:41:29 -0700

    - removing as many contact methods as I can
    It's not all of them but this is what's available in the array currently

--- lib/proud-customize-wp-profile.php
+++ lib/proud-customize-wp-profile.php
@@ -29,7 +29,35 @@ class Proud_Custom_Profile{
 	 */
 	public function init(){
         remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
+        add_filter( 'user_contactmethods', array( $this, 'remove_contact_methods' ), 99, 2 );
 	} // init
+
+    /**
+     * Removes a bunch of the available contact methods from the Contact Info area of the User Profile screen
+     *
+     * @since 2022.05.05
+     * @author Curtis McHale
+     *
+     * @param   $methods        array           required            array of existing methods
+     * @param   $user           object          optional            user object for the current user
+     * @return  $methods        array                               the contact methods array as empty as I can make it
+     */
+    public static function remove_contact_methods( $methods, $user ){
+
+        unset( $methods['facebook'] );
+        unset( $methods['dbem_phone'] );
+        unset( $methods['instagram'] );
+        unset( $methods['linkedin'] );
+        unset( $methods['myspace'] );
+        unset( $methods['pinterest'] );
+        unset( $methods['soundcloud'] );
+        unset( $methods['tumblr'] );
+        unset( $methods['twitter'] );
+        unset( $methods['youtube'] );
+        unset( $methods['wikipedia'] );
+
+        return $methods;
+    }
 
 }
 
